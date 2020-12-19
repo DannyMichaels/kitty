71604f0e77bbd95623ff527eeada3b4b137c11cf
Daniel Michael <itzdanielmichael@gmail.com>
2020-12-19 08:09:39 -0500

    same thing but with useref

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import "./Home.css";
 import { getCat } from "../../services/apiConfig";
 import Button from "../../components/shared/Styled/Button";
@@ -7,6 +7,8 @@ function Home() {
   const [count, setCount] = useState(0);
   const [catData, setCatData] = useState({});
   const [loading, setLoading] = useState(true);
+  const [isSending, setIsSending] = useState(false);
+  const isMounted = useRef(true);
 
   useEffect(() => {
     document.title = `${count} cats seen!`;
@@ -23,15 +25,25 @@ function Home() {
     getApi();
   }, []);
 
-  const fetchRequest = useCallback(() => {
-    const getApi = async () => {
-      const response = await getCat();
-      setCatData(response[0]);
-      setLoading(false);
-      setCount((currentCount) => (currentCount += 1));
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
     };
-    getApi();
   }, []);
+
+  const sendRequest = useCallback(async () => {
+    // don't send again while we are sending
+    if (isSending) return;
+    // update state
+    setIsSending(true);
+    // send the actual request
+    const response = await getCat();
+    // once the request is sent, update state again
+    setCatData(response[0]);
+    if (isMounted.current)
+      // only update if we are still mounted
+      setIsSending(false);
+  }, [isSending]); // update the callback if the state changes
 
   return (
     <div className="home">
@@ -45,7 +57,9 @@ function Home() {
         </div>
       </div>
 
-      <Button onClick={fetchRequest}>GET CAT</Button>
+      <Button disabled={isSending} onClick={sendRequest}>
+        GET CAT
+      </Button>
     </div>
   );
 }
