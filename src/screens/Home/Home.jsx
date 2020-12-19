52c0ced9248b45a27c31478340b2119efbcec876
Daniel Michael <itzdanielmichael@gmail.com>
2020-12-19 07:36:58 -0500

    the right way to reset count

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,50 +1,53 @@
-import React , { useState, useEffect } from 'react'
-import './Home.css'
-import { getCat } from '../../services/apiConfig'
-import Button from '../../components/shared/Styled/Button'
-
-
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import { getCat } from "../../services/apiConfig";
+import Button from "../../components/shared/Styled/Button";
 
 function Home() {
-  const [count, setCount] = useState(0)
-  const [catData, setCatData] = useState({})
-  const [loading, setLoading] = useState(true)
+  const [count, setCount] = useState(0);
+  const [catData, setCatData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    document.title = `${count} cats seen!`
-  }, [count])
- 
+    document.title = `${count} cats seen!`;
+  }, [count]);
+
   useEffect(() => {
-    const getApi  = async () => {
-      const response = await getCat()
-      setCatData(response[0])
-      setLoading(false)
-    }
-    getApi()
-  }, [])
+    const getApi = async () => {
+      const response = await getCat();
+      setCatData(response[0]);
+      setLoading(false);
+    };
+    getApi();
+  }, []);
 
   useEffect(() => {
     if (count + 1) {
       const getApi = async () => {
-        const response = await getCat()
-        setCatData(response[0])
-        setLoading(false)
-      }
-    getApi()
+        const response = await getCat();
+        setCatData(response[0]);
+        setLoading(false);
+      };
+      getApi();
     }
-  }, [count])
-
+  }, [count]);
 
   return (
     <div className="home">
       <div>
         <div>
-          { loading ? <div style={{fontSize: '36px'}}>LOADING CAT...</div> : catData && <img src={catData.url} alt="cat"/>}
+          {loading ? (
+            <div style={{ fontSize: "36px" }}>LOADING CAT...</div>
+          ) : (
+            catData && <img src={catData.url} alt="cat" />
+          )}
         </div>
-      </div>  
-    <Button onClick={() => setCount(count + 1)}>GET CAT</Button>
+      </div>
+      <Button onClick={() => setCount((currentCount) => currentCount + 1)}>
+        GET CAT
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
